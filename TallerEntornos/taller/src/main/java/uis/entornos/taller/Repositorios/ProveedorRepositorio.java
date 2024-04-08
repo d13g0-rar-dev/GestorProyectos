2cf36e04cd119f00a740c7fcd698e5dfa1c1e753
d13g0-rar-dev <diego2210063@correo.uis.edu.co>
2024-04-08 15:12:02 -0500

    Eliminacion de Proveedores

--- TallerEntornos/taller/src/main/java/uis/entornos/taller/Repositorios/ProveedorRepositorio.java
+++
@@ -1,8 +0,0 @@
-package uis.entornos.taller.Repositorios;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import uis.entornos.taller.Modelos.Proveedor;
-
-public interface ProveedorRepositorio extends JpaRepository<Proveedor, Integer>{
-
-}
