b908f7f70127b4ca44744030a126779729b7c61c
desgrac1aaa <decire2211835@correo.uis.edu.co>
2024-04-12 12:50:01 -0500

    Create login.js

---
+++ TallerEntornos/FrontEnd/js/login.js
@@ -0,0 +1,27 @@
+function validarLogin(){
+    let Correo = document.getElementById('icon_user').value;
+    let contraseña = document.getElementById('icon_pass').value;
+    
+    let data = {
+        'email': Correo,
+        'passwaord': contraseña
+    };
+    
+    let request = sendRequest('api/usuario/loginclient', 'POST', data);
+    
+    request.onload = function(){
+        let dataResponse = request.response;
+        console.log(dataResponse);
+
+        if(dataResponse === 1){
+            alert("Inicio de sesión éxitoso");
+            window.location = 'index.html';
+        } else {
+            alert('Correo o contraseña incorrectos.');
+        }
+    };
+
+    request.onerror = function(){
+        alert("Error al enviar la solicitud.");
+    };
+}
