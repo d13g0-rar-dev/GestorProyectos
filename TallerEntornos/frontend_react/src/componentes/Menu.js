8dfa734755207c58ecd41c9116e2c308ad86982c
d13g0-rar-dev <diego2210063@correo.uis.edu.co>
2024-06-22 17:53:25 -0500

    fixear nombre usuario en home

--- TallerEntornos/frontend_react/src/componentes/Menu.js
+++ TallerEntornos/frontend_react/src/componentes/Menu.js
@@ -7,7 +7,7 @@ const Menu = () => {
       <ul className="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
         <li className="nav-item">
           <Link to="/home" className="nav-link">
-            <i className="nav-icon fas fa-list" />
+            <i className="nav-icon fas fa-list fa-lg" style={{ fontSize: '24px', marginRight: '8px' }}/>
             <p>
               Inicio
             </p>
@@ -15,7 +15,7 @@ const Menu = () => {
         </li>
         <li className="nav-item">
           <Link to="/Proyectos-Admin" className="nav-link">
-            <i className="nav-icon fas fa-edit" />
+            <i className="nav-icon fas fa-edit fa-lg" style={{ fontSize: '24px', marginRight: '8px' }} />
             <p>
               Proyectos
             </p>
@@ -23,7 +23,7 @@ const Menu = () => {
         </li>
         <li className="nav-item">
           <Link to="/Tareas-Admin" className="nav-link">
-            <i className="nav-icon fas fa-tasks" />
+            <i className="nav-icon fas fa-tasks fa-lg" style={{ fontSize: '24px', marginRight: '8px' }}/>
             <p>
               Tareas
             </p>
--- TallerEntornos/frontend_react/src/componentes/SidebarContainer.js
+++ TallerEntornos/frontend_react/src/componentes/SidebarContainer.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Menu from "./Menu";
-import Logo from '../../node_modules/admin-lte/dist/img/AdminLTELogo.png'
+import Logo from '../../node_modules/admin-lte/dist/img/AdminLTELogo.png';
 
 const SidebarContainer = () => {
+
+  const storedData = localStorage.getItem('DatosUsuario');
+  console.log(storedData);
+
   return (
     <aside className="main-sidebar sidebar-dark-primary elevation-4">
       <Link to={"/home"} className="brand-link">
@@ -13,31 +17,9 @@ const SidebarContainer = () => {
           className="brand-image img-circle elevation-3"
           style={{ opacity: ".8" }}
         />
-        <span className="brand-text font-weight-light">Admin Proyecto</span>
+        <span className="brand-text font-weight-light">Hola {storedData}</span>
       </Link>
       <div className="sidebar">
-        <div className="user-panel mt-3 pb-3 mb-3 d-flex">
-          <div className="info">
-            <Link to={"/home"} className="d-block">
-              Menu Principal
-            </Link>
-          </div>
-        </div>
-        <div className="form-inline">
-          <div className="input-group" data-widget="sidebar-search">
-            <input
-              className="form-control form-control-sidebar"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <div className="input-group-append">
-              <button className="btn btn-sidebar">
-                <i className="fas fa-search fa-fw" />
-              </button>
-            </div>
-          </div>
-        </div>
         <Menu></Menu>
       </div>
     </aside>
