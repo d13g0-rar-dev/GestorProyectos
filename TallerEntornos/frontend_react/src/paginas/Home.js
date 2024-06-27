5be4a8961ad94b91f02f34597b12c34113044f08
d13g0-rar-dev <diego2210063@correo.uis.edu.co>
2024-06-26 20:42:26 -0500

    Modificacion Home Grupos
    Modificacion del componente Home que muestra los grupos actuales de cada miembro

--- TallerEntornos/frontend_react/src/paginas/Home.js
+++ TallerEntornos/frontend_react/src/paginas/Home.js
@@ -1,12 +1,32 @@
-import React from 'react'
+import {React, useEffect, useState} from 'react'
 import Navbar from '../componentes/Navbar'
 import SidebarContainer from '../componentes/SidebarContainer';
 import ContentHeader from '../componentes/ContentHeader';
 import Footer from '../componentes/Footer';
+import APIInvoke from '../utils/APIInvoke';
 import { Link } from 'react-router-dom';
 
 
 const Home = () => {
+
+   const usuario_id = JSON.parse(localStorage.getItem('DatosUsuario')).id;
+   const [grupos, setGrupos] = useState([]);
+
+   const cargarGrupos = async () => {
+      const response = await APIInvoke.invokeGET('/api/grupos/list')
+      const grupos = response.filter(grupo => grupo.members.some(member => member.id === usuario_id));
+      
+      console.log("grupos",grupos);
+      console.log("response",response);
+      setGrupos(grupos);
+   }
+   
+   useEffect(() => {
+      cargarGrupos();
+   }, [])
+
+   
+
    return (
       <div className="wrapper">
          <Navbar></Navbar>
@@ -20,36 +40,24 @@ const Home = () => {
             />
             <section className="content">
                <div className='container-fluid'>
-                  
-                  <div className='row'>
-                     <div className="col-lg-3 col-6">
-                        <div className="small-box bg-info">
-                           <div className="inner">
-                              <h3>Proyectos</h3>
-                              <p>&nbsp;</p>
+                  <div className="row">
+                     {grupos.map((grupo) => (
+                        <div className="col-12 col-sm-6 col-md-3" key={grupo.id}>
+                           <div className="info-box">
+                              <span className="info-box-icon bg-info elevation-1"><i className="fas fa-users fa-lg"></i></span>
+                              <div className="info-box-content">
+                                 <span className="info-box-text">{grupo.name}</span>
+                                 <span className="info-box-text">Codigo de Ingreso: {grupo.id}</span>
+                                 <span className="info-box-number">{grupo.members.length} Miembros</span>
+                                 <Link to={`/grupos/${grupo.id}`} className="btn btn-primary" >Ingresar</Link>
+                              </div>
                            </div>
-                           <div className="icon">
-                              <i className="nav-icon fas fa-edit" />
-                           </div>
-                           <Link to={"/Proyectos-Admin"}className="small-box-footer">Ver proyectos <i className="fas fa-arrow-circle-right" /></Link>
                         </div>
-                     </div>
-                     <div className="col-lg-3 col-6">
-                        <div className="small-box bg-success">
-                           <div className="inner">
-                              <h3>Tareas</h3>
-                              <p>&nbsp;</p>
-                           </div>
-                           <div className="icon">
-                              <i className="nav-icon fas fa-tasks" />
-                           </div>
-                           <Link to={"/Tareas-Admin"} className="small-box-footer">Ver tareas <i className="fas fa-arrow-circle-right" /></Link>
-                        </div>
-                     </div>
-                  </div>
+                     ))}
+                  </div>      
                </div>
             </section>
-         </div>
+            </div>
          <Footer></Footer>
       </div>
 
