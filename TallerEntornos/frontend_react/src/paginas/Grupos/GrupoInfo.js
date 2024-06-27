4b460a214087a8b3aec375834cd1112365ad7698
d13g0-rar-dev <diego2210063@correo.uis.edu.co>
2024-06-27 12:55:11 -0500

    Mapeo Tareas por Estado

--- TallerEntornos/frontend_react/src/paginas/Grupos/GrupoInfo.js
+++ TallerEntornos/frontend_react/src/paginas/Grupos/GrupoInfo.js
@@ -9,6 +9,9 @@ import APIInvoke from '../../utils/APIInvoke';
 const GrupoInfo = () => {
 
     const [grupos, setGrupos] = useState([]);
+    const [tareasPendientes, setTareasPendientes] = useState([]);
+    const [tareasProgreso, setTareasProgreso] = useState([]);
+    const [tareasFinalizadas, setTareasFinalizadas] = useState([]);
 
     const { idgrupo } = useParams();
     let arreglo = idgrupo.split('@');
@@ -16,6 +19,13 @@ const GrupoInfo = () => {
 
     const cargarGrupo = async () => {
         const grupos = await APIInvoke.invokeGET(`/api/grupos/list/${idGrupo}`);
+        const tareas = await APIInvoke.invokeGET(`/api/grupos/list/${idGrupo}/tasks`);
+        const tareasPendientes = tareas.filter(tarea => tarea.status === 'Pendiente');
+        const tareasProgreso = tareas.filter(tarea => tarea.status === 'En Curso');
+        const tareasFinalizadas = tareas.filter(tarea => tarea.status === 'Finalizada');
+        setTareasPendientes(tareasPendientes);
+        setTareasProgreso(tareasProgreso);
+        setTareasFinalizadas(tareasFinalizadas);
         setGrupos(grupos);
     }
 
@@ -31,106 +41,116 @@ const GrupoInfo = () => {
             <SidebarContainer></SidebarContainer>
             <div className="content-wrapper ">
                 <ContentHeader
-                    Titulo={[<i className="nav-icon fas fa-tasks fa-lg"></i>,' Listado Tareas']}
+                    Titulo={[<i className="nav-icon fas fa-tasks fa-lg"></i>, ' Listado Tareas']}
                     breadcrumb1={'Inicio'}
                     breadcrumb2={'Grupos'}
-                    ruta={'/home'}
-                />
+                    ruta={'/home'} />
                 <section className="content">
-                    <div className="container-fluid">
-                        <div className="row">
-                            <div className="col-md-6">
-                                <div className="card card-primary">
-                                    <div className="card-header">
-                                        <h3 className="card-title">
-                                            To Do
-                                        </h3>
-                                    </div>
-                                    <div className="card-body">
-                                        <div className="card card-primary card-outline">
-                                            <div className="card-header">
-                                                <h5 className="card-title">Crear primer milestone</h5>
-                                                <div className="card-tools">
-                                                    <a href="#" className="btn btn-tool btn-link">#5</a>
-                                                    <a href="#" className="btn btn-tool">
-                                                        <i className="fas fa-pen"></i>
-                                                    </a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
+                <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-6">
+                            <div className="card card-primary">
+                                <div className="card-header">
+                                    <h3 className="card-title">
+                                        Pendiente
+                                    </h3>
                                 </div>
-                            </div>
-                            <div className="col-md-6">
-                                <div className="card card-row card-default">
-                                    <div className="card-header bg-info">
-                                        <h3 className="card-title">
-                                            On Progress
-                                        </h3>
-                                    </div>
-                                    <div className="card-body">
-                                        <div className="card card-primary card-outline">
-                                            <div className="card-header">
-                                                <h5 className="card-title">Crear primer milestone</h5>
-                                                <div className="card-tools">
-                                                    <a href="#" className="btn btn-tool btn-link">#5</a>
-                                                    <a href="#" className="btn btn-tool">
-                                                        <i className="fas fa-pen"></i>
-                                                    </a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-md-6">
-                                <div className="card card-row card-success">
-                                    <div className="card-header">
-                                        <h3 className="card-title">
-                                            Done
-                                        </h3>
-                                    </div>
-                                    <div className="card-body">
-                                        <div className="card card-primary card-outline">
-                                            <div className="card-header">
-                                                <h5 className="card-title">Crear primer milestone</h5>
-                                                <div className="card-tools">
-                                                    <a href="#" className="btn btn-tool btn-link">#5</a>
-                                                    <a href="#" className="btn btn-tool">
-                                                        <i className="fas fa-pen"></i>
-                                                    </a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-md-6">
-                                <div className="card">
-                                    <div className="card-header">
-                                        <h3 className="card-title"><i className="fas fa-filter"></i>Opciones</h3>
-                                    </div>
-                                    <div className="card-body">
-                                        <h5><a className="d-block w-100" data-toggle="collapse" href="#detalles"><i className='fas fa-code'/> Ver detalles del grupo</a></h5>
-                                        <div id="detalles" className="panel-collapse collapse">
-                                            <div className="panel-body">
-                                                { 
-                                                    <>Nombre: {grupos.name}<p>Codigo de Ingreso: {grupos.id}</p></>
-                                                }
-                                            </div>
-                                        </div>
-                                        <h5><Link to={`/Tareas-Registro/${idGrupo}@`}><i className='fas fa-code'/>Crear Tarea</Link></h5>
-                                        <h5><Link to={'/home'}><i className='fas fa-code'/>Subir Documento</Link></h5>
-                                        <h5><Link to={'/home'}><i className='fas fa-code'/>Regresar</Link></h5>
-                                        
+                                <div className="card-body">
+                                    <div className="card card-primary card-outline">                                  
+                                        {
+                                            
+                                            tareasPendientes.map(tarea => 
+                                                <>
+                                                    <div className='card-header'>
+                                                        <h5 className='card-title'>{tarea.name}</h5>
+                                                        <div className="card-tools">
+                                                            <a href="#" className="btn btn-tool btn-link"><i className="fas fa-info"></i></a>
+                                                            <Link to={`/Tareas-editar/${tarea.id}@${tarea.name}@${tarea.description}@${tarea.date.split('T')[0]}@${tarea.deadline.split('T')[0]}/`} className='btn btn-sm btn-primary'><i className='fas fa-pen'></i></Link>
+                                                        </div>
+                                                    </div>
+                                                </>
+                                            )                                            
+                                        }
                                     </div>
                                 </div>
                             </div>
                         </div>
+                        <div className="col-md-6">
+                            <div className="card card-row card-default">
+                                <div className="card-header bg-info">
+                                    <h3 className="card-title">
+                                        On Progress
+                                    </h3>
+                                </div>
+                                <div className="card-body">
+                                    <div className="card card-primary card-outline">
+                                    {
+                                            
+                                        tareasProgreso.map(tarea => 
+                                            <>
+                                                <div className='card-header'>
+                                                    <h5 className='card-title'>{tarea.name}</h5>
+                                                    <div className="card-tools">
+                                                        <a href="#" className="btn btn-tool btn-link"><i className="fas fa-info"></i></a>
+                                                        <Link to={`/Tareas-editar/${tarea.id}@${tarea.name}@${tarea.description}@${tarea.date.split('T')[0]}@${tarea.deadline.split('T')[0]}/`} className='btn btn-sm btn-primary'></Link>
+                                                    </div>
+                                                </div>
+                                            </>
+                                        )                                            
+                                    }
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="card card-row card-success">
+                                <div className="card-header">
+                                    <h3 className="card-title">
+                                        Done
+                                    </h3>
+                                </div>
+                                <div className="card-body">
+                                    <div className="card card-primary card-outline">
+                                    {
+                                        tareasFinalizadas.map(tarea => 
+                                            <>
+                                                <div className='card-header'>
+                                                    <h5 className='card-title'>{tarea.name}</h5>
+                                                    <div className="card-tools">
+                                                        <a href="#" className="btn btn-tool btn-link"><i className="fas fa-info"></i></a>
+                                                        <Link to={`/Tareas-editar/${tarea.id}@${tarea.name}@${tarea.description}@${tarea.date.split('T')[0]}@${tarea.deadline.split('T')[0]}/`} className='btn btn-sm btn-primary'></Link>
+                                                    </div>
+                                                </div>
+                                            </>
+                                        )                                            
+                                    }
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="card">
+                                <div className="card-header">
+                                    <h3 className="card-title"><i className="fas fa-filter"></i>Opciones</h3>
+                                </div>
+                                <div className="card-body">
+                                    <h5><a className="d-block w-100" data-toggle="collapse" href="#detalles"><i className='fas fa-code' /> Ver detalles del grupo</a></h5>
+                                    <div id="detalles" className="panel-collapse collapse">
+                                        <div className="panel-body">
+                                            {<>Nombre: {grupos.name}<p>Codigo de Ingreso: {grupos.id}</p></>}
+                                        </div>
+                                    </div>
+                                    <h5><Link to={`/Tareas-Registro/${idGrupo}@`}><i className='fas fa-code' />Crear Tarea</Link></h5>
+                                    <h5><Link to={'/home'}><i className='fas fa-code' />Subir Documento</Link></h5>
+                                    <h5><Link to={'/home'}><i className='fas fa-code' />Regresar</Link></h5>
+
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                </section>
-            </div>
-        <Footer></Footer>
+                </div>
+            </section>
+        </div><Footer></Footer>
         </div>
     )
 };
